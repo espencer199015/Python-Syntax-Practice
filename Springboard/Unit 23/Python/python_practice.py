2506ba573df843e58b48e2357672a72ab37cf229
Liz Erlandson <elizabeth.erlandson1@gmail.com>
2023-04-12 22:09:05 -0400

    pushing python syntax exercise to github

---
+++ Springboard/Unit 23/Python/python_practice.py
@@ -0,0 +1,15 @@
+# num = 0
+
+# while num <= 100:
+    # print(num)
+    # num = num + 10
+
+# print("All Done!")
+
+# target = 37
+guess = None
+while guess != target:
+    guess = input("Please enter a guess....")
+    guess = int(guess)
+    
+print("All Done!")
---
+++ Springboard/Unit 23/practice.py
@@ -0,0 +1,7 @@
+def greet(person):
+    returnf"Hello there, {person}"
+    
+def divide(a,b):
+    if type(a) is int and type(b) is int:
+    return a/b
+    return 'a and b must be integers!'
